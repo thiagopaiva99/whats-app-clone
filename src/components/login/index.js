0bf0cbef15c88197d2640293160f8aebe1ac7388
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-07-28 19:32:41 -0300

    Little style on Login Component

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 
 export default props => (
-    <View>
-        <View>
-            <Text>WhatsApp Clone</Text>
+    <View style={{ flex: 1, padding: 10 }}>
+        <View style={{ flex: 1 }}>
+            <Text style={{ fontSize: 25, justifyContent: 'center', alignContent: 'center' }}>WhatsApp</Text>
         </View>
-        <View>
-            <TextInput placeholder='E-mail' />
-            <TextInput placeholder='Senha' />
-            <Text>Ainda não tem uma conta? Cadastre-se!</Text>
+        <View style={{ flex: 2 }}>
+            <TextInput style={{ fontSize: 20, height: 45 }}  placeholder='E-mail' />
+            <TextInput style={{ fontSize: 20, height: 45 }} visible-password='no' placeholder='Senha' />
+            <Text style={{ fontSize: 20 }}>Ainda não tem uma conta? Cadastre-se!</Text>
         </View>
-        <View>
-            <Button title='Logar' onPress={ () => false } />
+        <View style={{ flex: 2 }}>
+            <Button title='Logar' color='#115E54' onPress={ () => false } />
         </View>
     </View>
 );
