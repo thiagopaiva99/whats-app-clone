e9e118fdccd2f41b4c4e06b19c4eaefa383e72d4
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-07-28 19:10:58 -0300

    Setting the initial strucutre and first Component

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+
+export default class WhatsAppClone extends Component {
+  render() {
+    return <App />;
+  }
+}
