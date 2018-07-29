6379dce03d77592baef5e1d97fe0d6885f6f0959
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-07-28 22:47:40 -0300

    Adding the redux and react-redux, adding the provider to main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import Routes from './Routes';
 
 export default props => (
-    <Routes />
+    <Provider>
+        <Routes />
+    </Provider>
 );
