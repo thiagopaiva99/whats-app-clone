438ba426fd8fc044b3c4a4978dc1b2a34995c052
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-07-28 22:54:45 -0300

    Creating the application reducers file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import Routes from './Routes';
+import reducers from './reducers/index';
 
 export default props => (
-    <Provider>
+    <Provider store={ createStore(reducers) }>
         <Routes />
     </Provider>
 );
