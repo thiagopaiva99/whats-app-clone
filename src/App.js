5a948ea9dd12e6050dbfbc05a7ca4c4392fc6977
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-07-30 20:32:43 -0300

    Adding .env file config and creating the firebase config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 
 import Routes from './Routes';
 import reducers from './reducers/index';
 
-export default props => (
-    <Provider store={ createStore(reducers) }>
-        <Routes />
-    </Provider>
-);
+class App extends React.Component {
+    componentWillMount() {
+        const config = {
+            apiKey: process.env.API_KEY,
+            authDomain: process.env.AUTH_DOMAIN,
+            databaseURL: process.env.DATABASE_URL,
+            projectId: process.env.PROJECT_ID,
+            storageBucket: process.env.STORAGE_BUCKET,
+            messagingSenderId: process.env.MESSAGING_SENDER_ID
+        };
+
+        firebase.initializeApp(config);
+    }
+
+    render() {
+        return (
+            <Provider store={ createStore(reducers) }>
+                <Routes />
+            </Provider>
+        );
+    }
+}
+
+export default App;
