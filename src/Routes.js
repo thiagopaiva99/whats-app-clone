e14e4d699d7e7a6a73d627f1a4b5f37f0d03eacf
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-07-30 20:16:31 -0300

    Adding the firebase dep and hiding the Login and Register navbars

--- src/Routes.js
+++ src/Routes.js
@@ -7,8 +7,8 @@ import Register from './components/register/index';
 export default props => (
     <Router>
         <Scene key='root'>
-            <Scene key='login' component={ Login } title='Login' />
-            <Scene key='register' component={ Register } title='Register' />
+            <Scene key='login' component={ Login } title='Login' hideNavBar={ true } />
+            <Scene key='register' component={ Register } title='Register' hideNavBar={ true } />
         </Scene>
     </Router>
 )
