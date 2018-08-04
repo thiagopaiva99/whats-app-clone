f0c10ca4be90468cc23372d75292343aa2e28c69
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-08-04 08:34:48 -0300

    Creating the ContactReducer and importing it to combineReducers()

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import AuthReducer from './authReducer';
 import AppReducer from './appReducer';
+import ContactReducers from './contactReducers';
 
 export default combineReducers({
     AuthReducer,
-    AppReducer
+    AppReducer,
+    ContactReducers
 });
