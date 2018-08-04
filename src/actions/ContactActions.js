45885d69fc6ce4c1fe5ae6b7b11e6a9e299c1398
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-08-04 09:34:14 -0300

    Checking for the contact on firebase

--- src/actions/ContactActions.js
+++ src/actions/ContactActions.js
@@ -1,3 +1,10 @@
+import { Actions } from 'react-native-router-flux';
+import b64 from 'base-64';
+
+import firebase from '@firebase/app';
+import '@firebase/auth';
+import '@firebase/database';
+
 import { CONTACT_MODIFY_EMAIL } from '../constants';
 
 const modifyEmail = text => {
@@ -8,6 +15,18 @@ const modifyEmail = text => {
 }
 
 const addContact = email => {
+    const emailB64 = b64.encode(email);
+
+    firebase
+        .database()
+        .ref(`/contacts/${emailB64}`)
+        .once('value')
+        .then(snapshot => {
+            if (snapshot.val()) {
+                
+            }
+        })
+
     return {
         type: '',
         payload: email
