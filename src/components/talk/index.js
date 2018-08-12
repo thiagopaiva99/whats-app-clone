0efd4bae81714f73e9c0d5a1142bba6cb0ca3c0a
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-08-12 11:54:02 -0300

    Creating the Talk Component

---
+++ src/components/talk/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class Talk extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Conversa</Text>
+            </View>
+        )
+    }
+}
