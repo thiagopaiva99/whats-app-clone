a4534b79ce6c86e45644511190662fa0d6c448e4
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-08-12 11:47:23 -0300

    Styling the contacts list

--- src/components/contacts/index.js
+++ src/components/contacts/index.js
@@ -24,9 +24,9 @@ class Contacts extends React.Component {
     }
 
     _renderRow = data => (
-        <View>
-            <Text>{ data.name }</Text>
-            <Text>{ data.email }</Text>
+        <View style={{ flex: 1, padding: 20, borderBottomWidth: 1, borderBottomColor: '#CCC' }}>
+            <Text style={{ fontSize: 23 }}>{ data.name }</Text>
+            <Text style={{ fontSize: 18 }}>{ data.email }</Text>
         </View>
     )
 
