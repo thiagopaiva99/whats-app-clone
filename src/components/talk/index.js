847898b106a6fd58466fec60d182660d9e27b6f0
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-08-12 12:50:51 -0300

    Creating components of talk Component

--- src/components/talk/index.js
+++ src/components/talk/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, Image, TouchableHighlight } from 'react-native'
 
 class Talk extends React.Component {
     render() {
         return (
-            <View>
-                <Text>Conversa</Text>
+            <View style={{ flex: 1, marginTop: 50, backgroundColor: '#eee4dc', padding: 10 }}>
+                <View style={{ flex: 1, paddingBottom: 20 }}></View>
+                <View style={{ flexDirection: 'row', height: 60 }}>
+                    <TextInput 
+                        style={{ flex: 4, backgroundColor: '#fff', fontSize: 18 }} />
+
+                    <TouchableHighlight onPress={ () => false } underlayColor="transparent">
+                        <Image source={ require('../../assets/images/send.png') } />
+                    </TouchableHighlight>
+                </View>
             </View>
         )
     }
